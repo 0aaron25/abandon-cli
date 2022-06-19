6ea683a5a9c3d71f3c4a0aaf1482118fc5e19b11
liangjiahao <562330458@qq.com>
2022-06-19 17:04:57 +0800

    add [nodeVesion | PkgVesion | root|UserHome] Check

--- core/core/bin/index.js
+++ core/core/bin/index.js
@@ -3,7 +3,6 @@
 const importLocal = require("import-local")
 
 if (importLocal(__filename)) {
-	console.log(1111)
 	require("npmlog").info("cli", "abandon-cli正在使用本地版本")
 } else {
 	require("../lib")(process.argv.slice(2))
--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,13 +1,51 @@
 "use strict"
 
 const log = require("@abandon-cli/log")
+const color = require("colors/safe")
+const semver = require("semver")
+const pathExists = require("path-exists").sync
+const userHome = require("user-home")
+const pkg = require("../package")
+const constants = require("./const")
 
 function core() {
 	// TODO
-	checkPkgVersion()
+	try {
+		checkNodeVersion()
+		checkPkgVersion()
+		checkRoot()
+		checkUserHome()
+	} catch (error) {
+		log.error(error.message)
+	}
 }
 
-const pkg = require("../package")
+//检查是否有主目录
+function checkUserHome() {
+	if (!userHome || !pathExists(userHome)) {
+		throw new Error(color.red(`当前登录用户主目录不存在`))
+	}
+}
+
+//检查是否用root账户
+function checkRoot() {
+	const rootCheck = require("root-check")
+	rootCheck()
+}
+
+//检查node版本
+function checkNodeVersion() {
+	const currentNodeVersion = process.version
+	const lowestNodeVersion = constants.LOWEST_NODE_VERSION
+
+	if (!semver.gte(currentNodeVersion, lowestNodeVersion)) {
+		throw new Error(
+			color.red(`abandon-cli需要安装v${lowestNodeVersion}版本的node.js`)
+		)
+	}
+}
+
+//检查包版本
 function checkPkgVersion() {
 	log.notice("version", pkg.version)
 }
